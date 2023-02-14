95ceee3fe4faf35c9165d96697d1003809e08602
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-14 14:34:48 +0600

    added firebase crediantials

---
+++ navigation/CustomDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {createDrawerNavigator, DrawerContentScrollView} from '@react-navigation/drawer';
+import {MainLayout} from '../screens/MainLayout';
+
+const Drawer = createDrawerNavigator()
+
+const CustomDrawer = () =>{
+  return(
+    <View>
+      <Text>thisikdjfie</Text>
+    </View>
+  )
+}
+
+export default CustomDrawer;
+
