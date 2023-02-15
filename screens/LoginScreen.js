7330701f2d3f057315ff862ef461f6c5ad7c8531
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-15 12:31:32 +0600

    created styles container and view text

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Email'
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Password'
+          // value={}
+          // onChangeText={text =>}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+
+  },
+  input: {
+
+  },
+  buttonContainer: {
+
+  },
+  button: {
+
+  },
+  buttonOutline: {
+
+  },
+  buttonText:{
+    
+  },
+  buttonOutlineText: {
+
+  },
+})
