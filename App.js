a4e2b4db985b6c1f1ea9f6c56d47a48612d33fe5
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-15 12:15:20 +0600

    added react navigation and added home and login to default route

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <View>
-      <Text>{count}</Text>
-      <Button title="Increase" onPress={() => setCount(count + 1)} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{headerShown: false}} name="Log In" component={LoginScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+// })
