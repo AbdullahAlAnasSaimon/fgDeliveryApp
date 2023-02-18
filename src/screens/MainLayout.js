f6635a302f705e66b7160e83fadb48b13dc29216
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-18 12:58:10 +0600

    added header to home screen

--- src/screens/MainLayout.js
+++ src/screens/MainLayout.js
@@ -1,8 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
-import Animated from 'react-native-reanimated';
+import Animated, {useSharedValue, useAnimatedStyle, withTiming} from 'react-native-reanimated';
+import { connect } from 'react-redux';
+import { setSelectedTab } from '../stores/tab/tabActions';
 
-const MainLayout = ({drawerAnimationStyle}) => {
+import {Home } from '../screens';
+import { SIZES } from '../constants';
+import Header from '../components/Header';
+
+const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedTab}) => {
   return (
     <Animated.View
       style={{
@@ -13,11 +19,34 @@ const MainLayout = ({drawerAnimationStyle}) => {
         ...drawerAnimationStyle
       }}
     >
+      {/* Header */}
+      <Header
+        containerStyle={{
+          height: 50,
+          paddingHorizontal: SIZES.padding,
+          marginTop: 40,
+          alignItems: 'center'
+        }}
+      />
+      {/* Body content */}
+      {/* Footer */}
       <Text>Main Layout</Text>
     </Animated.View>
   )
 }
 
-export default MainLayout
 
-const styles = StyleSheet.create({})
+function mapStateToProps(state){
+  return {
+    selectedTab: state.tabReducer.selectedTab
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    setSelectedTab: (selectedTab) => { return dispatch
+      (setSelectedTab(selectedTab))}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainLayout)
