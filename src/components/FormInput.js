1bfb3646634a97621788107394ea58517a5d27f7
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-20 12:23:40 +0600

    added form input component

---
+++ src/components/FormInput.js
@@ -0,0 +1,46 @@
+import { Text, View } from 'react-native'
+import React from 'react';
+import { FONTS, SIZES, COLORS } from '../constants';
+import { TextInput } from 'react-native-gesture-handler';
+
+const FormInput = ({containerStyle, label, placeholder, inputStyle, prependComponent, appendComponent, onChange, secureTextEntry, keyboardType = "default", autoComplete = "off", autoCapitalize="none", errorMsg=""}) => {
+  return (
+    <View
+    style={{...containerStyle}}>
+      <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      }}
+      >
+        <Text style={{ color: COLORS.gray, ...FONTS.body4}}>{label}</Text>
+        <Text style={{color: COLORS.red, ...FONTS.body4}}>{errorMsg}</Text>
+      </View>
+
+      <View
+      style={{ 
+        flexDirection: 'row',
+        height: 55,
+        paddingHorizontal: SIZES.padding,
+        marginTop: SIZES.base,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.darkGray2
+      }}
+      >
+        {prependComponent}
+
+        <TextInput
+        style={{
+          flex: 1,
+          ...inputStyle
+        }}
+        placeholder={placeholder}
+        placeholderTextColor={COLORS.gray}
+        secureTextEntry={secureTextEntry}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default FormInput
