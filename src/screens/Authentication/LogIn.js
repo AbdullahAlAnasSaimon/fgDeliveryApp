7b0ff2d6b56b068999c56ec78b0ed88b062fcaab
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-20 22:43:54 +0600

    added form input for password in the login screen

--- src/screens/Authentication/LogIn.js
+++ src/screens/Authentication/LogIn.js
@@ -9,6 +9,9 @@ const LogIn = () => {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
+
+  const [showPass, setShowPass] = useState(false);
+
   return (
     <AuthLayout
       title={"Please Log In"}
@@ -42,6 +45,36 @@ const LogIn = () => {
           </View>
         }
         />
+
+        <FormInput
+        label={"Password"}
+        secureTextEntry={!showPass}
+        autoComplete="password"
+        containerStyle={{
+          marginTop: SIZES.radius
+        }}
+        onChange={(value) => setPassword(value)}
+        appendComponent={
+          <TouchableOpacity
+          style={{
+            width: 40,
+            alignItems: 'flex-end',
+            justifyContent: 'center'
+          }}
+          onPress={() => setShowPass(!showPass)}
+          >
+            <Image
+            source={showPass ? icons.eye_close : icons.eye}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.gray
+            }}
+            />
+          </TouchableOpacity>
+        }
+        />
+
       </View>
     </AuthLayout>
   )
