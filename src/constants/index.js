f82af260150a410088a8c584153cde30eae2a367
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-21 15:30:38 +0600

    added email input field with validation

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,4 +8,5 @@ export {
     COLORS,
     SIZES,
     FONTS,
+    icons
 }
--- src/screens/Authentication/SignUp.js
+++ src/screens/Authentication/SignUp.js
@@ -10,19 +10,50 @@ const SignUp = ({ navigation }) => {
   const [phoneNumber, setPhoneNumber] = useState("+880");
   const [password, setPassword] = useState("");
   const [showPass, setShowPass] = useState(false);
-  
+
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
   return (
     <AuthLayout
-    title="Getting Started"
-    subtitle="Create an account to continue"
-    titleContainerStyle={{
-      marginTop: SIZES.radius
-    }}
+      title="Getting Started"
+      subtitle="Create an account to continue"
+      titleContainerStyle={{
+        marginTop: SIZES.radius
+      }}
     >
-      
+      <View
+        style={{
+          flex: 1,
+          marginTop: SIZES.padding
+        }}>
+        <FormInput
+          label="Email"
+          keyboardType='email-address'
+          autoComplete='email'
+          onChange={(value) => {
+            utils.validateEmail(value, setEmailError);
+            setEmail(value)
+          }}
+          errorMsg={emailError}
+          appendComponent={
+            <View
+              style={{
+                justifyContent: 'center'
+              }}
+            >
+              <Image
+                source={email == "" || (email != "" && emailError == "") ? icons.correct : icons.cancel}
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor: email == "" ? COLORS.gray : (email != "" && emailError == "") ? COLORS.green : COLORS.red
+                }}
+              />
+            </View>
+          }
+        />
+      </View>
     </AuthLayout>
   )
 }
