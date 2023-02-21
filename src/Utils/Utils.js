c7d34a1291bfdd00d2b4d3d54519845b864f3e84
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-21 11:04:49 +0600

    modified some

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,6 +1,6 @@
 function isValidEmail(value) {
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(String(value).toLowerCase());
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(value).toLowerCase());
 }
 
 function validateEmail(value, setEmailError) {
--- src/screens/Authentication/LogIn.js
+++ src/screens/Authentication/LogIn.js
@@ -1,14 +1,16 @@
-import { Text, View, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import AuthLayout from './AuthLayout';
-import { SIZES, FONTS, COLORS } from '../../constants'
-import { FormInput } from '../../components'
+import { SIZES, FONTS, COLORS } from '../../constants';
+import { FormInput } from '../../components';
 import icons from '../../constants/icons';
+import { utils } from '../../utils';
+
 
 const LogIn = () => {
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
 
   const [showPass, setShowPass] = useState(false);
 
@@ -18,61 +20,64 @@ const LogIn = () => {
       subtitle="Welcome back, You have been missed"
     >
       <View
-      style={{
-        flex: 1,
-        marginTop: SIZES.padding * 2
-      }}>
+        style={{
+          flex: 1,
+          marginTop: SIZES.padding * 2
+        }}>
         <FormInput
-        label="Email"
-        keyboardType='email-address'
-        autoComplete='email'
-        onChange={(value) => setEmail(value)}
-        errorMsg={emailError}
-        appendComponent={
-          <View
-          style={{
-            justifyContent: 'center'
+          label="Email"
+          keyboardType='email-address'
+          autoComplete='email'
+          onChange={(value) => {
+            utils.validateEmail(value, setEmailError)
+            setEmail(value)
           }}
-          >
-            <Image 
-            source={icons.correct}
-            style={{
-              height: 20,
-              width: 20,
-              tintColor: COLORS.green
-            }}
-            />
-          </View>
-        }
+          errorMsg={emailError}
+          appendComponent={
+            <View
+              style={{
+                justifyContent: 'center'
+              }}
+            >
+              <Image
+                source={email == "" || (email != "" && emailError == "") ? icons.correct : icons.cancel}
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor: email == "" ? COLORS.gray : (email != "" && emailError == "") ? COLORS.green : COLORS.red
+                }}
+              />
+            </View>
+          }
         />
 
         <FormInput
-        label={"Password"}
-        secureTextEntry={!showPass}
-        autoComplete="password"
-        containerStyle={{
-          marginTop: SIZES.radius
-        }}
-        onChange={(value) => setPassword(value)}
-        appendComponent={
-          <TouchableOpacity
-          style={{
-            width: 40,
-            alignItems: 'flex-end',
-            justifyContent: 'center'
+          label={"Password"}
+          secureTextEntry={!showPass}
+          autoComplete="password"
+          containerStyle={{
+            marginTop: SIZES.radius
           }}
-          onPress={() => setShowPass(!showPass)}
-          >
-            <Image
-            source={showPass ? icons.eye_close : icons.eye}
-            style={{
-              width: 20,
-              height: 20,
-              tintColor: COLORS.gray
-            }}
-            />
-          </TouchableOpacity>
-        }
+          onChange={(value) => setPassword(value)}
+          appendComponent={
+            <TouchableOpacity
+              style={{
+                width: 40,
+                alignItems: 'flex-end',
+                justifyContent: 'center'
+              }}
+              onPress={() => setShowPass(!showPass)}
+            >
+              <Image
+                source={showPass ? icons.eye_close : icons.eye}
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: COLORS.gray
+                }}
+              />
+            </TouchableOpacity>
+          }
         />
 
       </View>
