3f0f9397e1811f9a9694654d6663e1d5a5404f74
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-02-23 21:31:42 +0600

    added salary to wallet

--- src/screens/Wallet.js
+++ src/screens/Wallet.js
@@ -1,7 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React, {useContext} from 'react';
+import { useDeliveryOrders } from '../hooks/useDeliveryOrders';
+import { StateContext } from '../context/AuthContext';
+import Loader from '../components/Loader';
+import { COLORS } from '../constants';
 
 const Wallet = () => {
+  const {user} = useContext(StateContext);
+  const [deliveryOrders, orderLoading, orderRefetch] = useDeliveryOrders(user?.email);
+
+  if(orderLoading){
+    return <Loader/>
+  }
+  orderRefetch();
+
+  const totalIncome = deliveryOrders?.data?.reduce(
+    (acc, item) => acc + item?.shipping_fee,
+    0
+  );
+
+  console.log(totalIncome);
+
   return (
     <View
     style={{
@@ -11,6 +30,13 @@ const Wallet = () => {
     }}
     >
       <Text>Current Balance</Text>
+      <Text
+      style={{
+        fontSize: 25,
+        fontWeight: 700,
+        color: COLORS.primary
+      }}
+      >{totalIncome} TK</Text>
     </View>
   )
 }
